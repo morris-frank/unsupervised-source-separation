6a2d49d6b9477c4a1d457be375b7bda0f7fe3561
Maurice Frank <maurice.frank@posteo.de>
2019-11-20 14:42:44 +0100

    Added the WaveNet decoder from NSynth + BlockConv1d test

--- thesis/test_functional.py
+++ thesis/test_functional.py
@@ -4,8 +4,8 @@ from .functional import time_to_batch, batch_to_time, shift1d
 
 
 def test_time_to_batch():
-    dilation = 8
     n_batch, n_channel, length = 8, 2, 32
+    dilation = 8
     x = torch.rand((n_batch, n_channel, length))
     ttb = time_to_batch(x, dilation)
     assert list(ttb.shape) == [n_batch * dilation, n_channel, length // dilation]
