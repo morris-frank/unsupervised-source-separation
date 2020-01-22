0e21d74dc930f1a4dee6173f6d2d1b219367ad45
Maurice Frank <maurice.frank@posteo.de>
2020-01-22 18:33:59 +0100

    Removed residual reference

--- nsynth/__init__.py
+++ nsynth/__init__.py
@@ -2,10 +2,8 @@ import matplotlib as mpl
 
 from .ae import WavenetAE
 from .config import make_config
-from .scheduler import ManualMultiStepLR
 from .vae import WavenetVAE
 
 mpl.use('Agg')
 
-__all__ = [WavenetAE, ManualMultiStepLR, make_config,
-           WavenetVAE]
+__all__ = [WavenetAE, make_config, WavenetVAE]
