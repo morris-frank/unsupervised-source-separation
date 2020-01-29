203e8afc26e159bb72d4d58147bc08346dae7ad2
Maurice Frank <maurice.frank@posteo.de>
2020-01-29 14:13:03 +0100

    Added invertible 1x1 convolution

--- thesis/nn/modules.py
+++ thesis/nn/modules.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
 
 from .functions import VectorQuantizationStraightThrough, VectorQuantization
 
@@ -27,3 +29,35 @@ class VQEmbedding(nn.Module):
         z_q_x_bar = z_q_x_bar_.permute(0, 2, 1).contiguous()
 
         return z_q_x, z_q_x_bar
+
+
+class ChannelConvInvert(nn.Module):
+    def __init__(self, channels: int):
+        super(ChannelConvInvert, self).__init__()
+        self.conv = nn.Conv1d(channels, channels, 1, bias=False)
+
+        # Init weights shall be orthonormal:
+        # this guarantees |det(weights)| == 1 but not the sign
+        weights = torch.qr(torch.empty(channels, channels).normal_())[0]
+
+        if torch.det(weights) < 0:
+            weights[:, 0] = -weights[:, 0]
+
+        self.conv.weight.data = weights.unsqueeze(-1)
+        self.inv_w = None
+
+    def forward(self, z: torch.Tensor, reverse: bool = False):
+        [n, _, l] = z.size()
+
+        w = self.conv.weight.squeeze()
+
+        if reverse:
+            if self.inv_w is None:
+                self.inv_w = Variable(w.type(z.dtype).inverse().unsqueeze(-1))
+            z = F.conv1d(z, self.inv_w, bias=None)
+            return z
+        else:
+            logdet_w = n * l * torch.logdet(w)
+            z = self.conv(z)
+
+            return z, logdet_w
