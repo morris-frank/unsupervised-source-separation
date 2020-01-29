3cfa7acab2c36a7cb3c97b304c94602911bc187f
Maurice Frank <maurice.frank@posteo.de>
2020-01-29 13:50:25 +0100

    removed dilated queue

--- thesis/nn/modules.py
+++ thesis/nn/modules.py
@@ -1,46 +1,7 @@
-from typing import Optional
-
 import torch
-from torch import dtype as torch_dtype
 from torch import nn
 
 from .functions import VectorQuantizationStraightThrough, VectorQuantization
-
-
-class DilatedQueue:
-    def __init__(self, size: int, data: Optional[torch.Tensor] = None,
-                 channels: int = 1, dilation: int = 1,
-                 dtype: torch_dtype = torch.float32):
-        self.idx_en, self.idx_de = 0, 0
-        self.channels, self.size = channels, size
-        self.dtype = dtype
-        self.dilation = dilation
-
-        self.data = data
-        if data is None:
-            self.reset()
-
-    def enqueue(self, x):
-        self.data[:, self.idx_en] = x
-        self.idx_en = (self.idx_en + 1) % self.size
-
-    def dequeue(self, num_deq=1):
-        start = self.idx_de - ((num_deq - 1) * self.dilation)
-        end = self.idx_de
-        if start < 0:
-            t1 = self.data[:, start::self.dilation]
-            t2 = self.data[:, self.idx_de % self.dilation:end:self.dilation]
-            t = torch.cat((t1, t2), 1)
-        else:
-            t = self.data[:, start:end:self.dilation]
-
-        self.idx_de = (self.idx_de + 1) % self.size
-        return t
-
-    def reset(self, device: str = 'cpu'):
-        self.idx_en, self.idx_de = 0, 0
-        self.data = torch.zeros((self.channels, self.size),
-                                dtype=self.dtype).to(device)
 
 
 class VQEmbedding(nn.Module):
