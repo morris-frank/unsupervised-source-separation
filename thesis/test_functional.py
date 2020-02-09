8d440edd496ddb5acde89a19e6f4815a4d7d89db
Maurice Frank <maurice.frank@posteo.de>
2020-02-09 20:03:16 +0100

    μlaw test

--- thesis/test_functional.py
+++ thesis/test_functional.py
@@ -3,8 +3,8 @@ import torch
 
 def test_μ_law():
     from .functional import encode_μ_law, decode_μ_law
-    x = torch.tensor([-1, -0.5, 0, 0.5, 0.9], dtype=torch.float)
-    y = torch.tensor([-128, -113, 0, 112, 125], dtype=torch.int8)
+    x = torch.tensor([-1, -0.5, 0, 0.5, 1.0], dtype=torch.float)
+    y = torch.tensor([-127, -111, 0, 111, 127], dtype=torch.int8)
 
     assert torch.all(encode_μ_law(x).type(torch.int8) == y)
     assert torch.allclose(decode_μ_law(y), x, atol=0.1)
