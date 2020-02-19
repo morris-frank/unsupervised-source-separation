17fde48733cb37c328b49054ffa15d45a99ac139
Maurice Frank <maurice.frank@posteo.de>
2020-02-19 15:32:32 +0100

    realnvp wrapper

--- thesis/data/wrapper.py
+++ thesis/data/wrapper.py
@@ -4,19 +4,15 @@ from os.path import abspath
 from torch import nn
 
 from . import Dataset
-from .toy import ToyDataSingle, ToyData, ToyDataSpectral
+from .toy import ToyData, ToyDataSpectral
 
 
 def map_dataset(model: nn.Module, data_dir: abspath, subset: str) -> Dataset:
-    if model.__class__.__name__ == "MultiRealNVP":
+    if model.__class__.__name__ in "RealNVP":
         wn_layers = model.params["kwargs"]["wn_layers"]
         receptive_field = int(2 * pow(2, wn_layers - 1))
         dset = ToyData(f"{data_dir}/toy_{subset}.npy", receptive_field)
-    elif model.__class__.__name__ in ("ConditionalRealNVP", "ExperimentalRealNVP"):
-        wn_layers = model.params["kwargs"]["wn_layers"]
-        receptive_field = int(2 * pow(2, wn_layers - 1))
-        dset = ToyDataSingle(f"{data_dir}/toy_{subset}.npy", receptive_field)
-    elif model.__class__.__name__ in ("WaveGlow", "ExperimentalWaveGlow"):
+    elif model.__class__.__name__ == "WaveGlow":
         wn_layers = model.params["kwargs"]["wn_layers"]
         receptive_field = int(pow(2, wn_layers - 1))
         dset = ToyData(f"{data_dir}/toy_{subset}.npy", receptive_field)
