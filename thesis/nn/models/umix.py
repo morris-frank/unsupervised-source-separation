f3b3d257277f0072546b207b7ac9de3334e7dfd4
Maurice Frank <maurice.frank@posteo.de>
2020-03-09 19:49:07 +0100

    multi-headed UMIx

--- thesis/nn/models/umix.py
+++ thesis/nn/models/umix.py
@@ -11,6 +11,53 @@ from ...functional import rsample_truncated_normal
 from ...utils import clean_init_args
 
 
+class q_sǀm(nn.Module):
+    def __init__(self, n_classes, mel_channels):
+        super(q_sǀm, self).__init__()
+        head_dim = 128
+        arm_dim = 32
+
+        self.f = Wavenet(
+            in_channels=1,
+            out_channels=head_dim,
+            n_blocks=1,
+            n_layers=11,
+            residual_channels=head_dim,
+            gate_channels=head_dim,
+            skip_channels=head_dim,
+            cin_channels=mel_channels,
+        )
+
+        self.f_k = nn.ModuleList()
+        self.f_k_μ = nn.ModuleList()
+        self.f_k_σ = nn.ModuleList()
+        for k in range(n_classes):
+            self.f_k.append(
+                Wavenet(
+                    in_channels=head_dim,
+                    out_channels=arm_dim,
+                    n_blocks=2,
+                    n_layers=11,
+                    residual_channels=arm_dim,
+                    gate_channels=arm_dim,
+                    skip_channels=arm_dim,
+                    cin_channels=mel_channels,
+                )
+            )
+            self.f_k_μ.append(nn.Conv1d(arm_dim, 1, 1))
+            self.f_k_σ.append(nn.Sequential(nn.Conv1d(arm_dim, 1, 1), nn.Softplus()))
+
+    def forward(self, m: torch.Tensor, m_mel: torch.Tensor):
+        μ, σ = [], []
+
+        f_m = self.f(m, m_mel)
+        for k in range(len(self.f_k)):
+            f_k = self.f_k[k](f_m, m_mel)
+            μ.append(self.f_k_μ[k](f_k))
+            σ.append(self.f_k_σ[k](f_k) + 1e-7)
+        return torch.cat(μ, dim=1), torch.cat(σ, dim=1)
+
+
 class UMixer(BaseModel):
     def __init__(self, mel_channels: int = 80):
         super(UMixer, self).__init__()
@@ -18,16 +65,7 @@ class UMixer(BaseModel):
 
         self.n_classes = 4
 
-        self.q_sǀm = Wavenet(
-            in_channels=1,
-            out_channels=2 * self.n_classes,
-            n_blocks=3,
-            n_layers=11,
-            residual_channels=256,
-            gate_channels=256,
-            skip_channels=256,
-            cin_channels=mel_channels,
-        )
+        self.q_sǀm = q_sǀm(self.n_classes, mel_channels)
 
         self.p_s = None
 
@@ -55,21 +93,23 @@ class UMixer(BaseModel):
         n_fft = 1024
         hop_length = 256
         sr = 16000
-        self.mel = MelSpectrogram(sample_rate=sr, n_fft=n_fft, hop_length=hop_length, n_mels=mel_channels, f_min=125, f_max=7600)
+        self.mel = MelSpectrogram(
+            sample_rate=sr,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            n_mels=mel_channels,
+            f_min=125,
+            f_max=7600,
+        )
 
     def forward(
         self, m: torch.Tensor, m_mel: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
 
         m_mel = self.upsample(m_mel)
-        m_mel = m_mel[:, :, :m.shape[-1]]
+        m_mel = m_mel[:, :, : m.shape[-1]]
 
-        η = self.q_sǀm(m, m_mel)
-        μ, σ = η.chunk(2, dim=1)
-
-        # yes?
-        μ = μ
-        σ = F.softplus(σ) + 1e-7
+        μ, σ = self.q_sǀm(m, m_mel)
 
         ŝ, log_q_ŝ = rsample_truncated_normal(μ, σ, ll=True)
 
@@ -78,9 +118,10 @@ class UMixer(BaseModel):
             ŝ_mel = self.upsample(self.mel(ŝ[:, k, :]))
             with torch.no_grad():
                 log_p_ŝ, _ = self.p_s[k](ŝ[:, None, k, :], ŝ_mel)
+                log_p_ŝ = log_p_ŝ.detach()[:, None]
 
             # Kullback Leibler for this k'th source
-            KL_k = - torch.mean(log_p_ŝ.detach()[:, None] - log_q_ŝ[:, k, :])
+            KL_k = -torch.mean(log_p_ŝ - log_q_ŝ[:, k, :])
             setattr(self.ℒ, f"KL_{k}", KL_k)
 
         m_ = self.p_mǀs(ŝ)
@@ -97,6 +138,7 @@ class UMixer(BaseModel):
         for k in range(self.n_classes):
             ℒ += β * getattr(self.ℒ, f"KL_{k}")
 
+        # ℒ = self.ℒ.KL_0
         return ℒ
 
     def upsample(self, c):
