Fixed posterior scaling + some plotting stuff

make.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 from thesis import plot
 from thesis.data.toy import ToyData, TOY_SIGNALS
-from thesis.io import load_model, get_newest_file
+from thesis.io import load_model, get_newest_file, save_append
 
 mpl.use("agg")
 
@@ -56,6 +56,16 @@ def make_separation_examples(data):
         plt.close()
 
 
+def make_posterior_examples(data, weights):
+    model = load_model(weights, "cpu")
+    dset = ToyData(path=f"{data}/test/", mel=True, sources=True)
+
+    for (m, mel), s in tqdm(dset):
+        (ŝ,) = model.q_s(m.unsqueeze(0), mel.unsqueeze(0)).mean
+        ŝ_mel = torch.cat([model.mel(ŝ[k, :])[None, :] for k in range(4)], dim=0)
+        save_append("./mean_posterior.pt", (ŝ.unsqueeze(1), ŝ_mel))
+
+
 def main(args):
     if args.weights is None:
         match = "*pt" if args.k is None else f"*{args.k}*pt"
@@ -76,6 +86,9 @@ def main(args):
             make_cross_likelihood_plot(args.data, args.k, args.weights)
     elif args.command == "separate":
         make_separation_examples(args.data)
+    elif args.command == "posterior":
+        with torch.no_grad():
+            make_posterior_examples(args.data, args.weights)
     else:
         raise ValueError("Invalid Command given")
 

show.py
@@ -8,31 +8,27 @@ from colorama import Fore
 from matplotlib import pyplot as plt
 
 from thesis import plot
-from thesis.data.toy import ToyDataSourceK, ToyDataRandomAmplitude, ToyData
-from thesis.functional import discretize
-from thesis.io import load_model, get_newest_file, exit_prompt
 from thesis.data.toy import TOY_SIGNALS
+from thesis.data.toy import ToyData
+from thesis.io import load_model, get_newest_file, exit_prompt
 from train import _load_prior_networks
 
 
 def show_sample(data, weights):
     model = load_model(weights, "cpu")
-    # model.p_s = _load_prior_networks(prefix='Mar22', device="cpu")
+    model.p_s = _load_prior_networks(prefix="Mar26", device="cpu")
 
     # dset = ToyDataRandomAmplitude(path=f"{data}/test/")
     dset = ToyData(path=f"{data}/test/", mel=True, sources=True)
 
     for (m, mel), s in dset:
-        # ŝ, m_, p_ŝ, log_q_ŝ = model.test_forward(m.unsqueeze(0), mel.unsqueeze(0))
-        # plot.toy.reconstruction(s, ŝ, m, m_)
-        # plot.toy.reconstruction(s, torch.cat(p_ŝ, 1))
-        μ_ŝ = model.q_s(m.unsqueeze(0), mel.unsqueeze(0)).mean
-        # μ_ŝ, _ = model.q_s(m.unsqueeze(0), mel.unsqueeze(0))  # For Gaussian
-        #s = discretize(s, model.μ).long()
-        # μ_ŝ = model.q_s(m.unsqueeze(0), mel.unsqueeze(0)).logits.argmax(
-        #     dim=-1
-        # )  # For categorical
-        _ = plot.toy.reconstruction(s, μ_ŝ, m)
+        ŝ, m_, p_ŝ, log_q_ŝ, α, β = model.test_forward(m.unsqueeze(0), mel.unsqueeze(0))
+        plot.toy.reconstruction(s, ŝ, m, m_)
+        plot.toy.reconstruction(m, m_, p_ŝ)
+        plot.toy.reconstruction(m, m_, log_q_ŝ)
+        plot.toy.reconstruction(m, m_, α, β)
+        # μ_ŝ = model.q_s(m.unsqueeze(0), mel.unsqueeze(0)).mean
+        # _ = plot.toy.reconstruction(s, μ_ŝ, m)
         plt.show()
         exit_prompt()
 
@@ -51,29 +47,63 @@ def show_cross_likelihood():
     plt.close()
 
 
-def show_log_p_z_test(data):
-    weights = "Mar22-0051_Flowavenet_sin_009425.pt"
-    model = load_model(f"./checkpoints/{weights}", "cpu")
-    dset = ToyDataSourceK(path=f"{data}/test/", k=0, mel=True)
-    dset_alt = ToyDataSourceK(path=f"{data}/test/", k=1, mel=True)
+def make_mel(signal):
+    from torchaudio.transforms import MelSpectrogram
+
+    mel_channels = 80
+    n_fft = 1024
+    hop_length = 256
+    sr = 16000
+    mel = MelSpectrogram(
+        sample_rate=sr,
+        n_fft=n_fft,
+        hop_length=hop_length,
+        n_mels=mel_channels,
+        f_min=125,
+        f_max=7600,
+    )
+    return mel(signal)
+
+
+def show_prior(data, source):
+    weights = get_newest_file("./checkpoints", f"*{source}*pt")
+    model = load_model(weights, "cpu").to("cpu")
     model.eval()
-    for i, ((s, m), (sz, mz)) in enumerate(zip(dset, dset_alt)):
-        s[:, 1100:] = sz[:, 1100:]
-        m[:, 5:] = mz[:, 5:]
-        s, m = s.unsqueeze(0), m.unsqueeze(0)
-        log_p, logdet = model(s, m)
 
-        s_ = s * torch.rand_like(s)
-        log_p_, logdet_ = model(s_, m)
+    dset = ToyData(path=f"{data}/test/", mel=True, sources=True, mel_sources=True)
 
-        print(
-            f"log_p|log_det: {log_p.detach().mean().item()}|{logdet.detach().item()}\tlog_p_|log_det_: {log_p_.detach().mean().item()}|{logdet_.detach().item()}"
-        )
-        _ = plot.toy.reconstruction(
-            torch.cat([s, s_], 1), torch.cat([log_p, log_p_], 1)
-        )
+    for (m, m_mel), (s, s_mel) in dset:
+        rand_s = torch.rand_like(m) * 0.1
+        rand_mel = make_mel(rand_s)
+
+        sig = torch.cat((s, m, rand_s), dim=0).unsqueeze(1)
+        mel = torch.cat((s_mel, m_mel.unsqueeze(0), rand_mel), dim=0)
+
+        log_p, _ = model(sig, mel)
+
+        plot.toy.reconstruction(sig, sharey=False)
+        plot.toy.reconstruction(log_p, sharey=False)
         plt.show()
-        input()
+        exit_prompt()
+
+
+def show_posterior(source):
+    weights = get_newest_file("./checkpoints", f"*{source}*pt")
+    model = load_model(weights, "cpu").to("cpu")
+    model.eval()
+
+    data = torch.load("./mean_posterior.pt")
+
+    for s, _ in data:
+        s_max = s.detach().squeeze().abs().max(dim=1).values[:, None, None]
+        s = s / s_max
+        s_mel = make_mel(s)
+        log_p, _ = model(s, s_mel.squeeze())
+
+        plot.toy.reconstruction(s, sharey=False)
+        plot.toy.reconstruction(log_p, sharey=False)
+        plt.show()
+        exit_prompt()
 
 
 def main(args):
@@ -87,12 +117,16 @@ def main(args):
         with torch.no_grad():
             show_sample(args.data, args.weights)
 
+    elif args.command == "posterior":
+        with torch.no_grad():
+            show_posterior(args.k)
+
     elif args.command == "cross-likelihood":
         show_cross_likelihood()
 
-    elif args.command == "logpz":
+    elif args.command == "prior":
         with torch.no_grad():
-            show_log_p_z_test(args.data)
+            show_prior(args.data, args.k)
 
     else:
         raise ValueError("Invalid command given")
@@ -104,4 +138,5 @@ if __name__ == "__main__":
     parser.add_argument("--weights", type=abspath)
     parser.add_argument("--device", type=str, default="cpu")
     parser.add_argument("--data", type=abspath, default="/home/morris/var/data/toy")
+    parser.add_argument("-k", type=str)
     main(parser.parse_args())

thesis/nn/models/umix.py
@@ -94,14 +94,20 @@ class UMixer(BaseModel):
         ŝ = q_s.mean
         log_q_ŝ = q_s.log_prob(ŝ)
 
+        # Scale the posterior samples so they fill range [-1, 1].
+        # This is necessary as we start around zero with the values and the
+        # prior distributions assign too high likelihoods around zero!
+        ŝ_max = ŝ.detach().squeeze().abs().max(dim=1).values[:, None, None]
+        ŝ = ŝ / ŝ_max
+
         p_ŝ = []
         for k in range(self.n_classes):
-            ŝ_mel = self.c_up(self.mel(ŝ[:, k, :]), m.shape[-1])
+            ŝ_mel = self.mel(ŝ[:, k, :]), m.shape[-1]
             log_p_ŝ, _ = self.p_s[k](ŝ[:, None, k, :], ŝ_mel)
             p_ŝ.append(log_p_ŝ)
 
         m_ = self.p_mǀs(ŝ)
-        return ŝ, m_, p_ŝ, log_q_ŝ
+        return ŝ, m_, torch.cat(p_ŝ, 1), log_q_ŝ, q_s.α, q_s.β
 
     def forward(
         self, m: torch.Tensor, m_mel: torch.Tensor

thesis/plot/toy.py
@@ -66,7 +66,7 @@ def squeeze(tensor):
     return tensor[:, 100:PRINT_LENGTH].numpy()
 
 
-def reconstruction(*signals: torch.Tensor):
+def reconstruction(*signals: torch.Tensor, sharey: bool = True):
     arguments = get_func_arguments()
     colores = ["k", "n", "y", "g", "r"]
     signals = list(map(squeeze, signals))
@@ -74,7 +74,7 @@ def reconstruction(*signals: torch.Tensor):
     N, hasM = max(ch), len(ch) >= 2
     ylim = (min(map(np.min, signals)), max(map(np.max, signals)))
 
-    fig, axs = plt.subplots(N + hasM, sharex="all", squeeze=False)
+    fig, axs = plt.subplots(N + hasM, sharex="all", sharey='none', squeeze=False)
     if not isinstance(axs, np.ndarray):
         axs = list(axs)
     for k, (signal, name) in enumerate(zip(signals, arguments)):
@@ -84,7 +84,11 @@ def reconstruction(*signals: torch.Tensor):
             c = colores[k % len(colores)]
             for i in range(N):
                 axs[i, 0].plot(signal[i, :], f"{c}-", label=name)
-                axs[i, 0].set_ylim(ylim)
+                if sharey:
+                    axs[i, 0].set_ylim(ylim)
+                # else:
+                #     axs[i, 0].relim()
+                #     axs[i, 0].autoscale_view()
     for ax in axs.flatten().tolist():
         ax.legend()
     return fig

thesis/utils.py
@@ -81,9 +81,11 @@ def remove_list_weight_norm(ml: nn.ModuleList) -> nn.ModuleList:
 
 
 def get_func_arguments():
-    func_name = str(inspect.stack()[1][3]).strip()
-    argument_string = inspect.stack()[2][4][0].strip()
-    argument_string = argument_string.split(func_name)[1][1:-1]
-
+    func_name = inspect.stack()[1].function.strip()
+    code_line = inspect.stack()[2].code_context[0].strip()
+    try:
+        argument_string = re.search(rf"{func_name}\((.*)\)", code_line)[1]
+    except TypeError:
+        import ipdb; ipdb.set_trace()
     arguments = re.split(r",\s*(?![^()]*\))", argument_string)
     return arguments