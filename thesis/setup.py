15980b818604e67a2a56403d4563699d79ba41f0
Maurice Frank <maurice.frank@posteo.de>
2020-03-29 22:14:24 +0200

    missed file

---
+++ thesis/setup.py
@@ -0,0 +1,7 @@
+import platform
+from os.path import abspath
+
+IS_HERMES = platform.node() == 'hermes'
+TOY_SIGNALS = ["sin", "square", "saw", "triangle"]
+
+DEFAULT_DATA = "/home/morris/var/data/toy" if IS_HERMES else abspath("../data/toy")
